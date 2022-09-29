c7cf185789bc67be5a55c5ef1975b0424fcad21c
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2022-09-29 04:29:28 -0300

    feat: add loading component

---
+++ src/components/BasicComponents/Loading/Loading.tsx
@@ -0,0 +1,14 @@
+import './Loading.css'
+
+const Loading = () => (
+  <div style={{ width: '100%', textAlign: 'center' }}>
+    <div className="loading" data-testid="rw-loading-test-id">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+)
+
+export default Loading
