a393d22fc002d889b7cb49d365a3a2f319a6d761
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2022-09-29 01:16:24 -0300

    feat: add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from './Star_Wars_logo.png'
+import './header.css'
+
+const Header = () => (
+    <div className="header">
+        <img src={logo} alt="Star Wars" />
+    </div>
+)
+
+export default Header
